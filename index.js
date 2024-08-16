52b35ec21032efe27c9898bd7b4248a997775079
shubhanshu74156 <shubhanshu.tiwari@ssipmt.com>
2024-08-16 12:23:54 +0530

    port change

--- index.js
+++ index.js
@@ -77,7 +77,7 @@ io.on('connection', (socket) => {
 });
 
 
-const PORT = process.env.PORT || 8010;
+const PORT = process.env.PORT || 8000;
 server.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}`);
 });
